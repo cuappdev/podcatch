d39e1d3673ceccb9f807da7d99f95527b273d3cf
Jma353 <jma353@cornell.edu>
2017-11-30 12:22:57 -0500

    More progress

--- utils/episode_utils.py
+++ utils/episode_utils.py
@@ -11,7 +11,20 @@ def create_connector():
     PODCAST_DB_HOST, PODCAST_DB_NAME)
   return connector
 
+def get_series(connector):
+  rows = \
+    connector.read_batch('series', start=0, end=None, interval_size=1000)
+  return rows
+
+def get_episodes_by_series_ids(connector, series_ids):
+  sql_queries = [
+      'SELECT * FROM episodes WHERE series_id = {}'.format(series_id)
+      for series_id in series_ids]
+  return connector.execute_batch(sql_queries)
+
 if __name__ == '__main__':
   conn = create_connector()
-  print conn.config
-  print conn.get_count('episodes')
+  series_ids = [series[2] for series in get_series(conn)]
+  print len(series_ids)
+  episodes_per_series = get_episodes_by_series_ids(conn, series_ids)
+  print len(episodes_per_series)
